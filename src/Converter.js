1bd791e15df88e369b6c65c664e7832d4de037d0
Jfontaine <jfontaine19@gmail.com>
2022-06-28 16:43:51 -0400

    Added a useEffect that returns the list of currencies in the selector

--- src/Converter.js
+++ src/Converter.js
@@ -1,26 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Converter.css";
 
 const BASE_URL = "http://api.exchangeratesapi.io/latest";
 
 function Converter() {
+    const [currencyOptions, setCurrencyOptions] = useState([]);
+    
+
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
+      });
   }, []);
 
   return (
     <div className="converter">
       <h1>Converter</h1>
       <div className="converter__initial">
-        <input type="text" placeholder="Amount" />
-        <button>Currency</button>
+        <input className="input" type="text" placeholder="Amount" />
+        <select className="select">
+          {currencyOptions.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
       </div>
 
       <div className="converter__final">
-        <input type="text" placeholder="Amount" />
-        <button>Currency</button>
+        <input className="input" type="text" placeholder="Amount" />
+        <select className="select">
+          <option>USD</option>
+          <option>BTC</option>
+          <option>GBP</option>
+        </select>
       </div>
     </div>
   );
