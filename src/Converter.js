5c40984e8f09d98e32d2ec9e29d919b537ad83c3
Jfontaine <jfontaine19@gmail.com>
2022-07-03 18:50:36 -0400

    changed the structer of the converter component. Added separate CurrencyRow component. Using different API

--- src/Converter.js
+++ src/Converter.js
@@ -1,41 +1,21 @@
 import React, { useEffect, useState } from "react";
 import "./Converter.css";
+import CurrencyRow from "./CurrencyRow";
 
-const BASE_URL = "http://api.exchangeratesapi.io/latest";
+
 
 function Converter() {
-    const [currencyOptions, setCurrencyOptions] = useState([]);
-    
-
-  useEffect(() => {
-    fetch(BASE_URL)
-      .then((res) => res.json())
-      .then((data) => {
-        setCurrencyOptions([data.base, ...Object.keys(data.rates)]);
-      });
-  }, []);
-
   return (
     <div className="converter">
       <h1>Converter</h1>
-      <div className="converter__initial">
-        <input className="input" type="text" placeholder="Amount" />
-        <select className="select">
-          {currencyOptions.map((option) => (
-            <option key={option} value={option}>
-              {option}
-            </option>
-          ))}
-        </select>
+      <div className="currencyRow1">
+        <CurrencyRow currencies={['USD', 'EUR', 'GBP', ]} amount={1} currency={'USD'}/>
       </div>
 
-      <div className="converter__final">
-        <input className="input" type="text" placeholder="Amount" />
-        <select className="select">
-          <option>USD</option>
-          <option>BTC</option>
-          <option>GBP</option>
-        </select>
+      <div>=</div>
+
+      <div className="currencyRow2">
+        <CurrencyRow currencies={['USD']} amount={1} currency={'USD'} />
       </div>
     </div>
   );
